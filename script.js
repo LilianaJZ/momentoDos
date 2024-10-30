6e90a74299a6ba048ac4f09dcc3d70ac44cac6e8
LilianaJZ <ljimenezz@unal.edu.co>
2024-10-30 00:40:59 -0500

    Aplicacion del 5% de descuento adicional sobre valor final u calculo de totalFinal

--- script.js
+++ script.js
@@ -24,74 +24,82 @@ while (contador < 3) {
         let precioBruto = 0;
         let numeroProductos = parseInt(prompt('Indique el numero de productos que se van a vender: '));
 
-         // Validar que el numero de productos ingresados sea un número válido
-            while (isNaN(numeroProductos) || numeroProductos <= 0) {
+        // Validar que el numero de productos ingresados sea un número válido
+        while (isNaN(numeroProductos) || numeroProductos <= 0) {
             // Volver a pedir el numero de productos
             numeroProductos = parseInt(prompt('Respuesta inválida. Por favor, ingrese un numero de productos válido.'))
-            }
+        }
 
 
         console.log(numeroProductos)
         for (let i = 0; i < numeroProductos; i++) {
             let precioProducto = parseFloat(prompt('Ingrese el precio del producto #' + (i + 1)))
-            
+
             // Validar que el precio ingresado sea un número válido
-            while (isNaN(precioProducto) || precioProducto <= 0) {     
-            // Volver a pedir el precio
-            precioProducto = parseFloat(prompt('Respuesta inválida. Por favor, ingrese un precio válido. \n Ingrese el precio del producto #' + (i + 1)))
+            while (isNaN(precioProducto) || precioProducto <= 0) {
+                // Volver a pedir el precio
+                precioProducto = parseFloat(prompt('Respuesta inválida. Por favor, ingrese un precio válido. \n Ingrese el precio del producto #' + (i + 1)))
             }
             precioBruto = precioBruto + precioProducto
         }
         console.log(precioBruto)
 
-         // 5. El sistema permitirá al cliente agregar productos hasta que indique que no quiere más, usando un ciclo while.
-        
+        // 5. El sistema permitirá al cliente agregar productos hasta que indique que no quiere más, usando un ciclo while.
+
         let adicionarProducto = parseInt(prompt('¿Desea agregar otro producto? 1.Sí 2.No '))
+        // Validar que la respuesta sea 1 o 2
+        while (adicionarProducto !== 1 && adicionarProducto !== 2) {
+            // Volver a pregutar si se desea agregar producto
+            adicionarProducto = parseInt(prompt('Respuesta inválida. Digite nuevamente su respuesta \n ¿Desea agregar otro producto? 1.Sí 2.No '));
+        }
+        //Ciclo while para agregar productos hasta que se indique lo contrario
+        while (adicionarProducto === 1) {
+            let precioIngresado = parseFloat(prompt('Ingrese el precio del producto:'));
+
+            // Validar que el precio ingresado sea un número válido
+            while (isNaN(precioIngresado) || precioIngresado <= 0) {
+                // Volver a pedir el precio
+                precioIngresado = parseFloat(prompt('Respuesta inválida. Por favor, ingrese un precio válido.'))
+            }
+            numeroProductos++;
+            precioBruto += precioIngresado;
+
+            // Se vuele a preguntar si se desea ingresar un nuevo producto
+            adicionarProducto = parseInt(prompt('¿Desea agregar otro producto? 1.Sí 2.No '));
+
             // Validar que la respuesta sea 1 o 2
             while (adicionarProducto !== 1 && adicionarProducto !== 2) {
-                // Volver a pregutar si se desea agregar producto
                 adicionarProducto = parseInt(prompt('Respuesta inválida. Digite nuevamente su respuesta \n ¿Desea agregar otro producto? 1.Sí 2.No '));
             }
-            //Ciclo while para agregar productos hasta que se indique lo contrario
-                while (adicionarProducto === 1) {
-                    let precioIngresado = parseFloat(prompt('Ingrese el precio del producto:'));
-                    
-                    // Validar que el precio ingresado sea un número válido
-                    while (isNaN(precioIngresado) || precioIngresado <= 0) {
-                             // Volver a pedir el precio
-                    precioIngresado = parseFloat(prompt('Respuesta inválida. Por favor, ingrese un precio válido.'))
-                    }
-                    numeroProductos ++;
-                    precioBruto += precioIngresado;
-                    
-                    // Se vuele a preguntar si se desea ingresar un nuevo producto
-                    adicionarProducto = parseInt(prompt('¿Desea agregar otro producto? 1.Sí 2.No '));
+        }
 
-                    // Validar que la respuesta sea 1 o 2
-                    while (adicionarProducto !== 1 && adicionarProducto !== 2) {
-                        adicionarProducto = parseInt(prompt('Respuesta inválida. Digite nuevamente su respuesta \n ¿Desea agregar otro producto? 1.Sí 2.No '));
-                    }
-                }
-                
-                console.log(numeroProductos)
-                console.log(precioBruto)
+        console.log(numeroProductos)
+        console.log(precioBruto)
 
-                
+
 
         // 2. Si el cliente compra más de 5 productos, se aplica un 10% de descuento.
-        let descuento=0;
-        if (numeroProductos>5){
-            descuento= numeroProductos*(10/100);
+        let descuento = 0;
+        if (numeroProductos > 5) {
+            descuento = numeroProductos * (10 / 100);
         }
         console.log(descuento);
+
         // 3. Se debe calcular el IVA (19%) sobre el total.
         let total;
         let iva;
-        iva= (precioBruto - descuento)* 0.19
-        total=precioBruto - descuento + iva;
+        iva = (precioBruto - descuento) * 0.19
+        total = precioBruto - descuento + iva;
 
-        // 4. Si el valor final supera $500,000, se aplica un 5% de descuento adicional.
+        console.log(iva);
+        console.log(total);
 
+         // 4. Si el valor final supera $500,000, se aplica un 5% de descuento adicional.
+         let descuentoAdicional;
+         if (total > 500000) {
+            descuentoAdicional= total* 0.05;
+        }
+        totalFinal= total- descuentoAdicional;
 
         break;
     } else {
